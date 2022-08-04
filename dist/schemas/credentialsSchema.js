7303a00eef31496d3e725a42fb196b5eae944fa8
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-04 14:55:45 -0300

    Preparando revisão de código

--- dist/schemas/credentialsSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-var credentialSchema = joi.object({
-    title: joi.string().required(),
-    url: joi.string().required(),
-    nickName: joi.string().required(),
-    password: joi.string().required(),
-    userId: joi.number().required()
-});
-export { credentialSchema };
--- src/schemas/networksSchema.ts
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const networkSchema = joi.object({
-    title: joi.string().required(),
-    networkName: joi.string().required(),
-    password: joi.string().required()
-});
-
-export { networkSchema };
